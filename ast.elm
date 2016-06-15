a5277dbd0758cc92af306f7d01b9c5f0d8f23daa
Joel Burget <joelburget@gmail.com>
2016-06-15 10:15:34 -0400

    A few more days of iteration.

--- ast.elm
+++
@@ -1,7 +0,0 @@
-port module Ast exposing (..)
-
-import Html exposing (span, text)
-
-port transform : (String -> msg) -> Sub msg
-
-main = span [] [text "Hello, World!"]
--- purescript/src/Main.purs
+++ purescript/src/Main.purs
@@ -1,4 +1,5 @@
 module Main where
+-- Goal: complete roundtrip `Syntax -> RawSelection -> Action -> RawDraftContentBlock`
 
 import Prelude (otherwise, (/), (>=), (+), (-), (*), (++), (==), (&&), pure, (<*>), (<$>), (<=), bind, show, class Eq)
 
@@ -85,55 +86,10 @@ cToI c = case I.fromString (C.toString c) of
   Just i -> i
   Nothing -> 0
 
--- need to make all these aware of location of cursor
-operate :: Syntax -> Path -> Action -> Either String Syntax
-operate (Plus l r) path action = case path of
-  PathCons StepLeft rest ->
-    Plus <$> operate l rest action <*> pure r
-  PathCons StepRight rest ->
-    Plus <$> pure l <*> operate r rest action
-  -- TODO maybe we need to also handle parens?
-  _ -> Left "ran out of steps when operating on Plus"
-
-operate (Hole name) (PathOffset o) (Typing char)
-  | name == "" && isDigit char = Right (SNumber (cToI char))
-  | name == "" && char == '(' = Right (Plus (Hole "l") (Hole "r"))
-  | otherwise = Right (Hole (name ++ C.toString char))
-operate (SNumber n) (PathOffset o) (Typing char)
-  | isDigit char = Right (SNumber (n * 10 + cToI char))
-operate (SNumber n) (PathOffset o) Backspace
-  | n >= 10 = Right (SNumber (n / 10))
-  | otherwise = Right (Hole "")
-
-operate _ _ _ = Left "had steps remaining at a leaf"
-
-data Tuple3 a b c = Tuple3 a b c
-
-operateForeign :: Foreign -> Foreign -> Foreign -> Foreign
-operateForeign syntax path action =
-  let allRead = Tuple3 <$> read syntax <*> read path <*> read action
-      result :: Either String Syntax
-      result = case allRead of
-                 Left err -> Left (show err)
-                 Right (Tuple3 syntax' path' action') -> operate syntax' path' action'
-  in case result of
-       Left err -> toForeign err
-       Right result' -> toObj (result' :: Syntax)
-
-operateJs :: Fn3 Foreign Foreign Foreign Foreign
-operateJs = mkFn3 operateForeign
-
 type EntityRange =
   { offset :: Int
   , length :: Int
   , key :: String
-  }
-
-type RawDraftContentBlock =
-  { key :: Maybe String
-    -- type :: 'unstyled'
-  , text :: String
-  , entityRanges :: Array EntityRange
   }
 
 data LightInline
@@ -142,12 +98,95 @@ data LightInline
   | InlineHole String InlineInfo
 
 type InlineInfo = { anchor :: Maybe Int , focus :: Maybe Int }
--- newtype LightBlock = LightBlock (Array LightInline)
 
--- type EditorState =
---   { syntax :: Syntax
---   , anchor ::
+type RawDraftContentBlock =
+  { key :: Maybe String
+    -- type :: 'unstyled'
+  , text :: String
+  , entityRanges :: Array EntityRange
+  }
+
+type BFromCState =
+  { currentOffset :: Int
+  -- XXX something more descriptive than String?
+  , anchorKey :: Maybe String
+  , anchorOffset :: Int
+  , focusKey :: Maybe String
+  , focusOffset :: Int
+  , text :: String
+  , entityRanges :: Array EntityRange
+  }
+
+-- type AnchorFocusInfo =
+--   { anchorKey :: Maybe String
+--   , anchorOffset :: Int
+--   , focusKey :: Maybe String
+--   , focusOffset :: Int
 --   }
+--
+-- type TextInfo =
+--   { currentOffset :: Int
+--   , text :: String
+--   , entityRanges :: Array EntityRange
+--   }
+
+blockFromContent :: Array LightInline -> ContentState
+blockFromContent inlines =
+  let initialState =
+        { currentOffset: 0
+        , text: ""
+        , entityRanges: []
+        , anchorKey: Nothing
+        , anchorOffset: 0
+        , focusKey: Nothing
+        , focusOffset: 0
+        }
+      accum :: BFromCState -> LightInline -> BFromCState
+      accum state (InlinePlus str info) = accumText str (accumAnchorFocus info)
+      accum state (InlineNumber str info) = accumText str (accumAnchorFocus info)
+      accum state (InlineHole str info) = accumText str (accumAnchorFocus info)
+
+      accumText :: String -> BFromCState -> BFromCState
+      accumText str state =
+        let tLen = length text
+            sLen = length str
+        in state
+             { currentOffset = state.currentOffset + length str
+             , text = state.text + str
+             , entityRanges = snoc state.entityRanges
+               { offset: length text
+               , length: length str
+               , key: state.tag -- XXX
+               }
+             }
+
+      accumAnchorFocus :: Key -> InlineInfo -> BFromCState -> BFromCState
+      accumAnchorFocus key info state =
+        let state' = case info.anchor of
+              Just i -> info
+                { anchorOffset = state.currentOffset + i
+                , anchorKey = key
+                }
+              Nothing -> info
+            state'' = case info.focus of
+              Just i -> info
+                { focusOffset = state'.currentOffset + i
+                , focusKey = key
+                }
+              Nothing -> info
+        in state''
+
+      finalState = foldl accum initialState inlines
+  in { block: { key: key
+              , text: finalState.text
+              , entityRanges: finalState.entityRanges
+              }
+     , selection: { anchorKey: finalState.anchorKey
+                  , anchorOffset: finalState.anchorOffset
+                  , focusKey: finalState.focusKey
+                  , focusOffset: finalState.focusOffset
+                  }
+     }
 
 getOffset :: Maybe Path -> Maybe Int
 getOffset path = case path of
@@ -203,5 +242,79 @@ contentFromSyntax syntax anchor focus =
             InlineHole str (inlineSelection 0 (length str) anchorOffset focusOffset)
           ]
 
-{-- blocksFromContent :: Array LightBlock -> Array RawDraftContentBlock --}
-{-- blocksFromContent lightBlocks --}
+
+-- need to make all these aware of location of cursor
+operate :: Syntax -> Path -> Action -> Either String Syntax
+operate (Plus l r) path action = case path of
+  PathCons StepLeft rest ->
+    Plus <$> operate l rest action <*> pure r
+  PathCons StepRight rest ->
+    Plus <$> pure l <*> operate r rest action
+  -- TODO maybe we need to also handle parens?
+  _ -> Left "ran out of steps when operating on Plus"
+
+operate (Hole name) (PathOffset o) (Typing char)
+  | name == "" && isDigit char = Right (SNumber (cToI char))
+  | name == "" && char == '(' = Right (Plus (Hole "l") (Hole "r"))
+  | otherwise = Right (Hole (name ++ C.toString char))
+operate (SNumber n) (PathOffset o) (Typing char)
+  | isDigit char = Right (SNumber (n * 10 + cToI char))
+operate (SNumber n) (PathOffset o) Backspace
+  | n >= 10 = Right (SNumber (n / 10))
+  | otherwise = Right (Hole "")
+
+operate _ _ _ = Left "had steps remaining at a leaf"
+
+data Tuple3 a b c = Tuple3 a b c
+
+-- type RawSelection = { anchor :: Path, focus :: Path }
+type RawSelection =
+  { anchorKey :: String
+  , anchorOffset :: Int
+  , focusKey :: String
+  , focusOffset :: Int
+
+  -- , isBackward: Boolean
+  -- , hasFocus: Boolean
+  }
+
+data Selection
+  = SpanningSelection Path Path
+  | AtomicSelection Path
+
+type ContentState =
+  { block :: RawDraftContentBlock
+  -- TODO understand selectionBefore / selectionAfter
+  , selection :: RawSelection
+  }
+
+rawOperate :: Syntax
+           -> RawSelection
+           -> Action
+           -> Either String ContentState
+rawOperate syntax rawSelection action =
+  let selection = rawSelectionToSelection rawSelection
+      syntax' = operate syntax selection action
+      -- TODO can probably get this from rawSelectionToSelection
+      content = case selection of
+        SpanningSelection l r -> contentFromSyntax syntax' l r
+        AtomicSelection x -> contentFromSyntax syntax' x x
+      contentState = blockFromContent content
+  in contentState
+
+rawSelectionToSelection :: RawSelection -> Selection
+
+-- TODO change this from "operate" to "rawOperate"
+rawOperateForeign :: Foreign -> Foreign -> Foreign -> Foreign
+rawOperateForeign contentState action =
+  let allRead = Tuple3 <$> read syntax <*> read path <*> read action
+      result :: Either String Syntax
+      result = case allRead of
+                 Left err -> Left (show err)
+                 Right (Tuple3 syntax' path' action') -> operate syntax' path' action'
+  in case result of
+       Left err -> toForeign err
+       Right result' -> toObj (result' :: Syntax)
+
+operateJs :: Fn3 Foreign Foreign Foreign Foreign
+operateJs = mkFn3 operateForeign
